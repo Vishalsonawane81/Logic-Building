59ec590ffa5117d07f293094ca95f30cf6720b9b
Vishal Shrikant Sonawane <vishalsonawane1181@gmail.com>
2025-11-28 12:59:46 +0530

    Add files via upload

---
+++ Program125.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h> //  for malloc and free
+
+void Display(int Arr[],int iSize)
+{
+    int iCnt = 0;
+    for(iCnt = 0; iCnt < iSize ; iCnt++)
+    {
+        printf("%d\n",Arr[iCnt]);
+    }
+}
+
+int main()
+{
+    int iLength = 0,iCnt = 0;
+    int * ptr = NULL;
+
+    printf("Enter Number of Elements :");
+    scanf("%d",&iLength);   //5
+
+    ptr = (int *)malloc(iLength * sizeof(int));  //5 * 4  //int * 4,4,4,4,4
+
+    if(ptr = NULL) /////Major Error( = )
+    {
+        printf("Unable to Allocate the memmory\n");
+        return -1;
+    }
+
+    printf("Enter the Elements :\n");
+    for(iCnt = 0; iCnt < iLength;iCnt++)
+    {
+        scanf("%d",&ptr[iCnt]);
+    }
+    
+    Display(ptr,iLength);
+
+    free(ptr);
+   
+    return 0;
+}
---
+++ Program129.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h> //  for malloc and free
+
+int Summation(int Arr[],int iSize)
+{
+    int iCnt = 0,isum = 0;
+
+    for(iCnt = 0; iCnt < iSize ; iCnt++)
+    {
+        
+    }
+    return isum;
+}
+
+int main()
+{
+    int iLength = 0,iCnt = 0,iRet = 0;
+    int * ptr = NULL;
+
+    printf("Enter Number of Elements :");
+    scanf("%d",&iLength);   //5
+
+    ptr = (int *)malloc(iLength * sizeof(int));  //5 * 4  //int * 4,4,4,4,4
+
+    if(NULL == ptr) //Industial way of codeing
+    {
+        printf("Unable to Allocate the memmory\n");
+        return -1;
+    }
+
+    printf("Enter the Elements :\n");
+    for(iCnt = 0; iCnt < iLength;iCnt++)
+    {
+        scanf("%d",&ptr[iCnt]);
+    }
+    
+    iRet = Summation(ptr,iLength);
+
+    printf("Addition is : %d\n",iRet);
+
+    free(ptr);
+   
+    return 0;
+}
