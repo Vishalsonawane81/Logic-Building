e336778e5610658acc653794ba9b15afc2a54adc
Vishal Shrikant Sonawane <vishalsonawane1181@gmail.com>
2025-12-06 21:30:12 +0530

    Add files via upload

---
+++ Program156.java
@@ -0,0 +1,37 @@
+//Input  : 5
+//Output : # * # * # *
+//counter: 1 2 3 4 5 6
+
+import java.util.*;
+
+class Pattern
+{
+    public void Display(int iNo)
+        {
+           int iCnt = 0;
+
+           for(iCnt = iNo; iCnt >= 1; iCnt--)
+           {
+            
+            System.out.println(iCnt+"\t");
+
+           }
+           System.out.println("\t");
+        }
+    
+}
+
+class Program156
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue = 0;
+
+        System.out.println("Enter the Frequency ;");
+        iValue = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue);
+    }
+}
